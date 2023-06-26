ce916e00af4c1fec48265c3b1d9e9b0a98ed7ab9
Oskitar-Ale <oscarvilca66@gmail.com>
2023-06-25 22:52:25 -0500

    changed routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => res.render("index"));
+router.get("/login", (req, res) => res.render("login"));
+router.get("/register", (req, res) => res.render("register"));
 
 export default router;
