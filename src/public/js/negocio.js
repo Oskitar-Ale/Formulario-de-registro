6ab75c09f4f78fbfaad6700b34f436627dbfb389
Oskitar-Ale <oscarvilca66@gmail.com>
2023-06-27 23:54:22 -0500

    added a js folder

---
+++ src/public/js/negocio.js
@@ -0,0 +1,37 @@
+var listaEstudiante=[];
+
+function addamigostoSystem(inpnombre, inpapellidos, inpdni, incelular, inemail){
+    var newestudiante={
+        nombre : inpnombre,
+        apellidos : inpapellidos,
+        dni : inpdni,
+        celular : incelular,
+        email : inemail
+    };
+    console.log(newestudiante);
+    listaEstudiante.push(newestudiante);
+
+    // localStorageEstudiabtes(listaEstudiante);
+}
+
+// function ObtenerEstudiantes(){
+//     var storeList = localStorage.getItem("localEstudiantesList");
+    
+//     if (storeList==null){
+//         listaEstudiante=[];
+//     }else{
+//         listaEstudiante=JSON.parse(storeList);
+//     }
+
+//     return listaEstudiante;
+// }
+
+
+
+// // Storage
+
+// function localStorageEstudiabtes(plista){
+//     localStorage.setItem("localEstudiantesList", JSON.stringify(plista));
+    
+//     console.log(JSON.stringify(plista))
+//     }
