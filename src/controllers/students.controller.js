65a65d390ce3cabd99db513814260b3ff45580d3
Oskitar-Ale <oscarvilca66@gmail.com>
2023-07-01 23:56:48 -0500

    insertion of data to the registration panel

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,52 +1,45 @@
 import { pool } from "../db/connection.js";
 
-export const getStudents = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM students')
-    res.send(rows)
-};
-
-export const getStudent2 = async(req,res) =>{
-    const [rows] = await pool.query('SELECT * FROM students WHERE dni = ?',[req.params.id])
-    console.log(rows)
-    
-    if (rows.length <= 0) return res.status(404).json({
-        message:'Student not found'
-    });
-    
-    res.json(rows[0])
+export const getPanel = async(req, res) => {
+    const [data] = await pool.query('SELECT * FROM students');
+    // console.log(data);
+    res.render('panel',{ data })
 }
 
-
-export const createStudents = async (req, res) => {
-    const {dni, nombre, apellidos, celular, email} = (req.body)
-    const [rows] = await pool.query('INSERT INTO students (dni, nombre, apellidos, celular, email) VALUES (?,?,?,?,?)', [dni, nombre, apellidos, celular, email])
-    res.send({
-        dni, 
-        nombre, 
-        apellidos, 
-        celular, 
-        email,
-    })
-};
-
-export const updateStudents = (req, res) => res.send('actualizando empleados');
-export const deleteStudents = (req, res) => res.send('eliminando empleados');
-
-
 export const createPanel = async (req, res) => {
-    const {nombre, apellidos, dni, celular, email} = (req.body);
-    const newData = {
-        dni,
-        nombre,
-        apellidos,
-        celular,
-        email
-    };
-    if(nombre.trim() === '' || apellidos.trim() === '' || dni.trim() === ''){
-        console.log("El nombre y los apellidos no pueden estar vacíos.");
-    }else{
-        console.log(newData);
-        await pool.query('INSERT INTO students set ?', [newData]);
-        res.send('received')
+    try{
+        const {nombre, apellidos, dni, celular, email} = (req.body);
+        const newData = {
+            dni,
+            nombre,
+            apellidos,
+            celular,
+            email
+        };
+        console.log(`
+        nombre: ${nombre}
+        apellidos: ${apellidos}
+        dni: ${dni}
+        celular: ${celular}
+        email: ${email}
+        `)
+        if(nombre.trim() === '' || apellidos.trim() === '' || dni.trim() === ''){
+            console.log("El nombre y los apellidos no pueden estar vacíos.");
+            res.redirect('panel')
+        }else{
+            console.log(newData);
+            await pool.query('INSERT INTO students set ?', [newData]);
+            res.redirect('panel')
+        }
+    }catch(error){
+        console.error(error);
+        res.redirect('error');
     }
+    
 };
+
+export const deletePanel = async (req, res) => {
+    const {dni} = req.params;
+    await pool.query('DELETE FROM students WHERE dni = ?', [dni])
+    res.redirect('panel')
+}
