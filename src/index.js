0e5d0d68e282ec3b983873eecce28574dff8b646
Oskitar-Ale <oscarvilca66@gmail.com>
2023-07-05 23:27:13 -0500

    added settings for alerts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import  express  from "express";
-import morgan from "morgan";
-import bodyParser from 'body-parser';
+import  express  from 'express';
+import morgan from 'morgan';
 
+import bodyParser from 'body-parser';
+import flash from 'connect-flash';
+import session from 'express-session';
 import  {dirname, join} from 'path';
 import  {fileURLToPath} from 'url';
 import indexRoutes from './routes/index.js'; //== Import index routes ==
@@ -10,21 +12,37 @@ import requestsRoutes from './routes/requests.routes.js'; // == Import requests 
 
 //== Initialization==
 const app = express()
-
 app.use(express.json())
+
 //== Settings ==
 const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// == Motor ejs ==
 app.set('view engine','ejs');
 app.set('views', join(__dirname, 'views'))
 
+//== Middleware ==
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(session({
+    secret: 'mysecret', 
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(flash())
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
 
+//== Variables ==
+app.use((req,res,next) => {
+    res.locals.createS = req.flash('createSuccess');
+    res.locals.deleteS = req.flash('deleteSuccess');
 
-//== Route / == 
+    res.locals.error = req.flash('error');
+    next()
+}); 
+
+//== Route  == 
 app.use(indexRoutes)
 app.use(studentsRoutes)
 app.use(requestsRoutes) 
