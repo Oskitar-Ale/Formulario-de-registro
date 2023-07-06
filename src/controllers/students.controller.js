f7d0418c5935e30bf554373fd415e8c52cab7d5c
Oskitar-Ale <oscarvilca66@gmail.com>
2023-07-05 23:08:52 -0500

    added alerts to the buttons

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -23,13 +23,14 @@ export const createPanel = async (req, res) => {
         celular: ${celular}
         email: ${email}
         `)
-        if(nombre.trim() === '' || apellidos.trim() === '' || dni.trim() === ''){
+        if(nombre.trim() === '' || apellidos.trim() === '' || dni.trim() === ''|| celular.trim() === '' || email.trim() === '' ){
             console.log("El nombre y los apellidos no pueden estar vacíos.");
-            res.redirect('panel')
+            res.redirect('/panel')
         }else{
             console.log(newData);
             await pool.query('INSERT INTO students set ?', [newData]);
-            res.redirect('panel')
+            req.flash('createSuccess', 'Se registro exitosamente.');
+            res.redirect('/panel')
         }
     }catch(error){
         console.error(error);
@@ -43,12 +44,19 @@ export const deletePanel = async (req, res) => {
     const {dni} = req.params;
     console.log('se elimino el usuario:', dni);
     await pool.query('DELETE FROM students WHERE dni = ?', [dni]);
+    req.flash('deleteSuccess', 'Se elimino exitosamente.');
     res.redirect('/panel');
 }
 export const editPanel = async (req, res) => {
-    
-    const {dni} = req.params;
-    console.log('se edito el usuario:', dni);
-    await pool.query('UPDATE FROM students WHERE dni = ?', [dni]);
-    res.redirect('/panel');
+
+    const {dni, nombre, apellidos, email, celular } = req.body;
+    const newEdit = {dni, nombre, apellidos, email, celular}
+    console.log(`
+    se edito el usuario: ${dni}
+    Nuevos datos actualizados: ${JSON.stringify(newEdit)}`);
+    await pool.query('UPDATE students set ? WHERE dni = ?', [newEdit, dni]);
+    res.redirect('/panel')
+
+    // const {dni} = req.params;
+    // res.redirect('/panel');
 }
