933dca0d5b31c0b83a2a697dcb4bf4562ee4e0ce
Oskitar-Ale <oscarvilca66@gmail.com>
2023-07-15 01:14:55 -0500

    added authentication for /panel

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 const  router =  Router();
 import {createPanel, getPanel, deletePanel, editPanel} from "../controllers/students.controller.js";
+import { isLoggedIn } from '../lib/auth.js';
 
 
-router.get('/panel', getPanel);
+router.get('/panel', isLoggedIn, getPanel);
 router.post('/panel/create/', createPanel);
 router.get('/panel/delete/:dni',deletePanel)
 router.post('/panel/edit/',editPanel)
